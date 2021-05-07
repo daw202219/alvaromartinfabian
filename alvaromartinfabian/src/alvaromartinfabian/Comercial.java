454a90d3e7494001428998a5008ed5594121bde7
admin <admin@Win7_1TB>
2021-05-07 11:22:28 +0300

    Creado mi branch y añadido el dato fecha de nacimiento

--- alvaromartinfabian/src/alvaromartinfabian/Comercial.java
+++ alvaromartinfabian/src/alvaromartinfabian/Comercial.java
@@ -1,5 +1,7 @@
 package alvaromartinfabian;
 
+import java.util.Date;
+
 public class Comercial {
 
 	protected double precio;
@@ -11,11 +13,43 @@ public class Comercial {
 		this.nombre = nombre;
 	}
 
+	
+	
+	public String getEntrada() {
+		return entrada;
+	}
+
+
+
+	public void setEntrada(String entrada) {
+		this.entrada = entrada;
+	}
+
+
+
+	public DateFormat getFormat() {
+		return format;
+	}
+
+
+
+	public void setFormat(DateFormat format) {
+		this.format = format;
+	}
+
+
+
 	@Override
 	public String toString() {
 		return "Comercial [precio=" + precio + ", nombre=" + nombre + "]";
 	}
 	
-
+	//Las siguientes líneas van comentadas debido a problemas de compilación
 	
+	String entrada = "25/06/1999"; // Metemos la fecha de nacimiento
+	DateFormat format = new SimpleDateFormat("DD/MM/YYYY"); // Necesitamos crear el formato de fecha
+	opc = sc.nextInt();
+	System.out.println(fecha);
+	
+	}
 }
