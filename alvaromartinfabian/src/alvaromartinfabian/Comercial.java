d2b195daeed5850b1497d7c2f724cc0d536c07a0
admin <admin@Win7_1TB>
2021-05-07 10:27:24 +0300

    Creado nombre, sueldo y su constructor

--- alvaromartinfabian/src/alvaromartinfabian/Comercial.java
+++ alvaromartinfabian/src/alvaromartinfabian/Comercial.java
@@ -2,4 +2,13 @@ package alvaromartinfabian;
 
 public class Comercial {
 
+	protected double precio;
+	protected String nombre;
+	
+	public Comercial(double precio, String nombre) {
+		super();
+		this.precio = precio;
+		this.nombre = nombre;
+	}
+	
 }
